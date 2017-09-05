7869e3d4a8cb2828f37dce564c4df3427f693f6f
MingxianFu <oxford.ming@gmail.com>
2017-09-05 15:38:50 -0500

    adding 2 images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Oxford Chinese Church';
+  fullImagePath: string;
+
+  constructor(){
+      console.log("mmmm");
+      this.fullImagePath = '/assets/images/cross.jpg';
+  }
+
+  ngOnInit(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,74 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { FellowshipComponent } from './fellowship/fellowship.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeCComponent } from './welcome-c/welcome-c.component';
+import { FellowshipCComponent } from './fellowship-c/fellowship-c.component';
+import { TestimonyCComponent } from './testimony-c/testimony-c.component';
+import { ScheduleCComponent } from './schedule-c/schedule-c.component';
+import { SchDataService } from './sch-data.service';
+import { FridayScheComponent } from './friday-sche/friday-sche.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    TestimonyComponent,
+    FellowshipComponent,
+    WelcomeComponent,
+    WelcomeCComponent,
+    FellowshipCComponent,
+    TestimonyCComponent,
+    ScheduleCComponent,
+    FridayScheComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'welcome',
+        component: WelcomeComponent
+      },
+      {
+        path: 'fellowship',
+        component: FellowshipComponent
+      },
+      {
+        path: 'testimony',
+        component: TestimonyComponent
+      },
+      {
+        path: 'schedule',
+        component: ScheduleComponent
+      },
+      {
+        path: 'welcomeC',
+        component: WelcomeCComponent
+      },
+      {
+        path: 'fellowshipC',
+        component: FellowshipCComponent
+      },
+      {
+        path: 'testimonyC',
+        component: TestimonyCComponent
+      },
+      {
+        path: 'scheduleC',
+        component: ScheduleCComponent
+      }     
+    ])
+
   ],
-  providers: [],
+  providers: [SchDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
